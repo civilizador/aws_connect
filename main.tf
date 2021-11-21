65dc094b04dd280636ac042821a4308326495bad
Asim Salimli <asimsalimli@Asims-MBP.attlocal.net>
2021-11-21 13:11:54 -0500

    added IAM role for Lambda function

--- main.tf
+++ main.tf
@@ -21,15 +21,16 @@ resource "aws_vpc" "aws_connect" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Creating an IAM policy for Lambda function
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "lambda_policy"
-  role = aws_iam_role.role_for_LDC.id
-  policy = file("policies/policy.json")
-}
 
 # Create IAM ROLE for the Lambda function. 
 resource "aws_iam_role" "aws_lambda_dynamo_admin_role" {
   name               = "terraform_function_role"
   assume_role_policy =  file("policies/assume_role_policy.json")
+}
+
+# Creating an IAM policy for Lambda function
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.aws_lambda_dynamo_admin_role.id
+  policy = file("policies/policy.json")
 }
