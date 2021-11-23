204d93dd1d943834382c7e4c5ff43040b030a55f
Asim Salimli <asimsalimli@Asims-MBP.attlocal.net>
2021-11-23 01:57:52 -0500

    reverted latest addition

--- lex_bot.tf
+++ lex_bot.tf
@@ -1,6 +1,6 @@
 
-resource "aws_lex_bot" "lex_bot_1" {
-  instance_id    = awsconnect_instance.connect_instance_1.instance_id
-  lex_bot_region = "us-east-1"
-  lex_bot_name   = "LexBotForConnectInstance1"
-}
+# resource "aws_lex_bot" "lex_bot_1" {
+#   instance_id    = awsconnect_instance.connect_instance_1.instance_id
+#   lex_bot_region = "us-east-1"
+#   lex_bot_name   = "LexBotForConnectInstance1"
+# }
