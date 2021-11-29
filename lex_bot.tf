f4e6e2f50b230a6fc88e590cfee440a4b66a82c5
ASIM SALIMLI <34362280+civilizador@users.noreply.github.com>
2021-11-28 22:41:51 -0500

    Added sample lex bot

--- lex_bot.tf
+++ lex_bot.tf
@@ -1,6 +1,33 @@
+resource "aws_lex_bot" "sample_bot" {
+  abort_statement {
+    message {
+      content      = "Sorry, I am not able to assist at this time"
+      content_type = "PlainText"
+    }
+  }
 
-# resource "aws_lex_bot" "lex_bot_1" {
-#   instance_id    = awsconnect_instance.connect_instance_1.instance_id
-#   lex_bot_region = "us-east-1"
-#   lex_bot_name   = "LexBotForConnectInstance1"
-# }
+  child_directed = false
+
+  clarification_prompt {
+    max_attempts = 2
+
+    message {
+      content      = "I didn't understand you, what would you like to do?"
+      content_type = "PlainText"
+    }
+  }
+
+  create_version              = false
+  description                 = "Bot to place an order on the behalf of a user"
+  idle_session_ttl_in_seconds = 600
+
+  intent {
+    intent_name    = "PlaceOrder"
+    intent_version = "1"
+  }
+
+  locale           = "en-US"
+  name             = "PlaceOrder"
+  process_behavior = "BUILD"
+  voice_id         = "Salli"
+}
